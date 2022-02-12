8aa8ca2c07448a21c5234ddb210b27b8a83d731b
Devanshu <devanshudesai_1509@yahoo.com>
2022-02-12 16:43:55 +0530

    added previous css

--- src/Tour.js
+++ src/Tour.js
@@ -10,12 +10,12 @@ const Tour = ({id, name, info, price, image, deleteTour}) => {
         <footer>
             <div className='tour-info'>
                 <h4>{name}</h4>
+            </div>
                 <div className='tour-price'>${price}</div>
-            <p>{readmore ? info : `${info.substring(0, 200)}...`}
+            <p className='description'>{readmore ? info : `${info.substring(0, 200)}...`}
                 <button onClick={()=> setReadmore(!readmore)}>{readmore ? 'show less' : 'read more'}</button>
             </p>
             <button className='delete-btn' onClick={() => deleteTour(id)}>Skip this trip...</button>
-            </div>
         </footer>
     </article>
   )
